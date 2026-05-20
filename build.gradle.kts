43b7b94bdcc6bb33da4ef2252e5452f1dad0539f
mossadceo <maglonium@gmail.com>
2026-05-20 07:44:11 +0300

    Migrate Gradle build to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    java
+}
+
+group = "net.mossadceo"
+version = "1.1.0"
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+    options.release.set(25)
+    options.compilerArgs.add("-Xlint:deprecation")
+}
+
+tasks.jar {
+    archiveBaseName.set("piss")
+}
